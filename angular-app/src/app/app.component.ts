2ecf4e957984f36b7ebff0d02e9c4f0499aa9e48
root <alexballe35@gmail.com>
2021-06-02 14:32:45 +0200

    update

--- angular-app/src/app/app.component.ts
+++ angular-app/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { firebase } from 'firebase/app';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  
+  // Your web app's Firebase configuration
+  firebaseConfig = {
+    apiKey: "AIzaSyCVKxK8FpwU2q9ru5xh4moCFKEMusZgFBk",
+    authDomain: "angular-efe7e.firebaseapp.com",
+    projectId: "angular-efe7e",
+    storageBucket: "angular-efe7e.appspot.com",
+    messagingSenderId: "19167489317",
+    appId: "1:19167489317:web:2da2bdcf8593a6539cbecc"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
 }
